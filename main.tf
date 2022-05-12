11db53301a3f6423cfd8a0fc25dc6fcfdc107763
Brett Stephen <igadget777@gmail.com>
2022-05-12 09:50:39 -0400

    add s3 backend

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 terraform {
-  # cloud {
-  #   organization = "stebdev"
-
-  #   workspaces {
-  #     name = "gh-actions-resume"
-  #   }
-  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.10.0"
+      version = "4.13.0"
     }
   }
+
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = aws_s3_bucket.this.name
+    key    = aws_s3_bucket.this.key
+  }
+
+
 }
